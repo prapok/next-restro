c3ce805bde3aabb6ebe2bf43014dbbb770fdf906
Praveen <prapok.se@gmail.com>
2020-08-19 10:45:09 +0200

    initial commit

--- next.config.js
+++ next.config.js
@@ -8,7 +8,8 @@ module.exports = {
     publicRuntimeConfig: {
         API_URL: process.env.API_URL,
     },
-
+    // Target must be serverless
+    target: 'serverless',
     webpack: config => {
         config.resolve.alias['components'] = path.join(__dirname, 'components')
         config.resolve.alias['public'] = path.join(__dirname, 'public')
